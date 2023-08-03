020b260afbf666f85b57bd68837b79263927406c
tukasingura <tukasingurarichard551@gmail.com>
2023-08-04 00:03:57 +0300

    third commit on the calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,19 +4,20 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cal_layout)
+        setContentView(R.layout.activity_main)
 
-        val image=findViewById<ImageView>(R.id.image)
-        val firstNumberEditText=findViewById<TextInputEditText>(R.id.first_number)
-        val secondNumberEditText=findViewById<TextInputEditText>(R.id.second_number)
+        val imageView=findViewById<ImageView>(R.id.image)
+        val firstNumberEditText=findViewById<EditText>(R.id.first_number)
+        val secondNumberEditText=findViewById<EditText>(R.id.second_number)
 
 
         val sumButton=findViewById<Button>(R.id.subtraction)
@@ -26,20 +27,51 @@ class MainActivity : AppCompatActivity() {
 
         val answerText=findViewById<TextView>(R.id.answer)
 
-        val firstNumber=firstNumberEditText.text.toString().toInt()
-        val secondNumber=secondNumberEditText.text.toString().toDouble()
 
-
+//giving the buttons in the xml functionality
         additionButton.setOnClickListener{
+
+            val firstNumber=firstNumberEditText.text.toString().toInt()
+            val secondNumber=secondNumberEditText.text.toString().toDouble()
             val sum= firstNumber + secondNumber
-            answerText.setText("the sum is $sum")
+            answerText.text="the sum is $sum"
 
             //either you can use this to display the same
-            //answerText.text="the sum is $sum"
+            //answerText.getText=("the sum is $sum")
         }
-        // changing the image to another one on the display
+        sumButton.setOnClickListener{
+
+            val firstNumber=firstNumberEditText.text.toString().toInt()
+            val secondNumber=secondNumberEditText.text.toString().toDouble()
+            val sub= firstNumber - secondNumber
+            answerText.text="the difference is $sub"
+        }
+        divisionButton.setOnClickListener{
+
+            val firstNumber=firstNumberEditText.text.toString().toInt()
+            val secondNumber=secondNumberEditText.text.toString().toDouble()
+            val div= firstNumber / secondNumber
+            answerText.text="the dividend is $div"
+
+        }
+        multiplicationButton.setOnClickListener{
+
+            val firstNumber=firstNumberEditText.text.toString().toInt()
+            val secondNumber=secondNumberEditText.text.toString().toDouble()
+            val mult= firstNumber * secondNumber
+            answerText.text="the product is $mult"
+
+        }
+//        // changing the image to another one on the display
+//        answerText.setOnClickListener{
+//            image.setImageResource(R.drawable.technology)
+//        }
+
+        // changing the image randomly from a set of image(array)
         answerText.setOnClickListener{
-            image.setImageResource(R.drawable.technology)
+            val imageArray= arrayOf(R.drawable.computericons,R.drawable.technology,R.drawable.image,R.drawable.internet)
+            imageView.setImageResource(imageArray.random())
         }
+
     }
 }
