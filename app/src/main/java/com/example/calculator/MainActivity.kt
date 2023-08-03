744a37797d2a03dd7f8ed17768123816ceacf146
tukasingura <tukasingurarichard551@gmail.com>
2023-08-03 18:37:02 +0300

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cal_layout)
+
+        val image=findViewById<ImageView>(R.id.image)
+        val firstNumberEditText=findViewById<TextInputEditText>(R.id.first_number)
+        val secondNumberEditText=findViewById<TextInputEditText>(R.id.second_number)
+
+
+        val sumButton=findViewById<Button>(R.id.subtraction)
+        val additionButton=findViewById<Button>(R.id.addition)
+        val divisionButton=findViewById<Button>(R.id.division)
+        val multiplicationButton=findViewById<Button>(R.id.multiplication)
+
+        val answerText=findViewById<TextView>(R.id.answer)
+
+        val firstNumber=firstNumberEditText.text.toString().toInt()
+        val secondNumber=secondNumberEditText.text.toString().toDouble()
+
+
+        additionButton.setOnClickListener{
+            val sum= firstNumber + secondNumber
+            answerText.setText("the sum is $sum")
+
+            //either you can use this to display the same
+            //answerText.text="the sum is $sum"
+        }
+        // changing the image to another one on the display
+        answerText.setOnClickListener{
+            image.setImageResource(R.drawable.technology)
+        }
     }
 }
